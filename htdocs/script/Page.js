8523b7e6a4c42e9e056d082e5e6c2a27cd776989
Notanypotato <a@a.a>
2014-10-26 20:08:22 -0400

    Fixed indentation/other issues with PencilTool and Page

--- htdocs/script/Page.js
+++ htdocs/script/Page.js
@@ -1,11 +1,11 @@
 (function (PictoSwap) {
-	//A class to hold the ink usage and data per page instead of in global arrays.
+    //A class to hold the ink usage and data per page instead of in global arrays.
         PictoSwap.Page = function () {
-                var inkUsage=0, empty=true, data=[];
-		return {
+            var inkUsage=0, empty=true, data=[];
+            return {
                 inkUsage: inkUsage,
                 empty: empty,
                 data: data,
-		};
-	};
+            };
+        };
 }(window.PictoSwap = window.PictoSwap || {}));
--- htdocs/script/PencilTool.js
+++ htdocs/script/PencilTool.js
@@ -1,77 +1,73 @@
 (function (PictoSwap) {
-	PictoSwap.PencilTool = function (canvas, previewCanvas,inkMeter) {
-	var pencilElement;
-	var canvas = canvas, previewCanvas = previewCanvas, inkMeter=inkMeter;
-	var drawing = false, drawColour = 'black';
+    PictoSwap.PencilTool = function (canvas, previewCanvas,inkMeter) {
+        var canvas = canvas, previewCanvas = previewCanvas, inkMeter=inkMeter;
+        var drawing = false, drawColour = 'black';
 
-	var x,y,lastX,lastY;
+        var x,y,lastX,lastY;
+ 
+        function onCanvasMousedown(e, currentPage){
+            // Amount of ink this page uses
+            currentPage.inkUsage += 1;
 
-		function onCanvasMousedown(e, currentPage){
+            // Begin a stroke
+            drawing = true;
+            canvas.beginStroke();
+            previewCanvas.beginStroke();
 
-                    if (pages[currentPage].empty) {
-                        saveButton.innerHTML = 'Save'; //TODO: Find a way to have this be called by the current tool
-                    }
+            // Draw dot
+            // We cache x and y to avoid bizzare browser bugs
+            // Don't ask me why, but the second time you read e.layerX, it becomes zero!
+            var x = e.layerX, y = e.layerY;
+            canvas.addDot(drawColour, x, y);
+            previewCanvas.addDot(drawColour, x, y);
 
-                    // Begin a stroke
-                    drawing = true;
-                    canvas.beginStroke();
-                    previewCanvas.beginStroke();
-
-                    // Draw dot
-                    // We cache x and y to avoid bizzare browser bugs
-                   // Don't ask me why, but the second time you read e.layerX, it becomes zero!
-                    var x = e.layerX, y = e.layerY;
-                    canvas.addDot(drawColour, x, y);
-                    previewCanvas.addDot(drawColour, x, y);
-
-                    // Store the position at present so that we know where next
-                    // segment will start
-                    lastX = e.layerX;
-                    lastY = e.layerY;
-		}
-		function onCanvasMousemove(e, currentPage){
-                    // We cache x and y to avoid bizzare browser bugs
-                    // Don't ask me why, but the second time you read e.layerX, it becomes zero!
-                   var x = e.layerX, y = e.layerY;
+            // Store the position at present so that we know where next
+            // segment will start
+            lastX = e.layerX;
+            lastY = e.layerY;
+        }
+        function onCanvasMousemove(e, currentPage){
+            // We cache x and y to avoid bizzare browser bugs
+            // Don't ask me why, but the second time you read e.layerX, it becomes zero!
+            var x = e.layerX, y = e.layerY;
             
-                    // Prevent mouse moving causing drawing on desktop
-                    // (Obviously, "mousemove" can't fire when not dragging on 3DS)
-                    if (drawing && (lastX !== x || lastY !== y)) {
-                        // The length of this segment will be subtracted from our "ink"
-                        var inkUsed = calcDistance(lastX, lastY, x, y);
+            // Prevent mouse moving causing drawing on desktop
+            // (Obviously, "mousemove" can't fire when not dragging on 3DS)
+            if (drawing && (lastX !== x || lastY !== y)) {
+                // The length of this segment will be subtracted from our "ink"
+                var inkUsed = calcDistance(lastX, lastY, x, y);
 
-                        // Only allow drawing if we have enough ink
-                        if (!inkMeter.subtractInk(inkUsed)) {
-                            return;
-                        }
-
-                        // Amount of ink this page uses
-                        currentPage.inkUsage += inkUsed;
-
-                        // Draw line
-                        canvas.addLine(drawColour, lastX, lastY, x, y);
-                        previewCanvas.addLine(drawColour, lastX, lastY, x, y);
-                    }
-
-                    lastX = x;
-                    lastY = y;
-		}
-                function onCanvasMouseup(e, currentPage){
-                    if (drawing) {
-                        onCanvasMousemove(e, currentPage);
-
-                        // End of stroke
-                        canvas.endStroke();
-                        previewCanvas.endStroke();
-                
-                        drawing = false;
-                    }
+                // Only allow drawing if we have enough ink
+                if (!inkMeter.subtractInk(inkUsed)) {
+                    return;
                 }
-	
-		return {
-                    onCanvasMousedown:onCanvasMousedown,
-                    onCanvasMousemove:onCanvasMousemove,
-                    onCanvasMouseup:onCanvasMouseup,
-		};
-	};
+
+                // Amount of ink this page uses
+                currentPage.inkUsage += inkUsed;
+
+                // Draw line
+                canvas.addLine(drawColour, lastX, lastY, x, y);
+                previewCanvas.addLine(drawColour, lastX, lastY, x, y);
+            }
+
+            lastX = x;
+            lastY = y;
+        }
+        function onCanvasMouseup(e, currentPage){
+            if (drawing) {
+                onCanvasMousemove(e, currentPage);
+
+                // End of stroke
+                canvas.endStroke();
+                previewCanvas.endStroke();
+                
+                drawing = false;
+            }
+        }
+        return {
+            onCanvasMousedown:onCanvasMousedown,
+            onCanvasMousemove:onCanvasMousemove,
+            onCanvasMouseup:onCanvasMouseup,
+        };
+    };
 }(window.PictoSwap = window.PictoSwap || {}));
